10d100e8878be1950a4a40a5366c261612c7e237
denis_stepanok <elfdenis@gmail.com>
2025-03-23 09:29:43 +0200

    done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,29 @@
 // import { FaRegThumbsUp } from 'react-icons/fa';
 // import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 // import { GiTreeDoor } from 'react-icons/gi';
-// import  StatisticsItem  from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import StatisticsItem from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-const Statistics = () => {
-  return <h2>Statistics</h2>;
+// const icons = {
+//   thumbsUp: FaRegThumbsUp,
+//   people: MdPeople,
+//   production: MdOutlineProductionQuantityLimits,
+//   tree: GiTreeDoor,
+// };
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <>
+      <h3 className={style.title}>{title}</h3>
+      <ul className={style.list}>
+        {stats.map(stat => (
+          <li key={stat.id} className={style.item}>
+            <StatisticsItem title={stat.title} total={stat.total} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Statistics;
